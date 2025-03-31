ac9e0e77270abc3ed96d7e31db06f9d2385acb9b
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-03-31 21:49:17 +0530

    Implement search functionality with debounce; enhance SearchBar component for controlled input

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -5,9 +5,13 @@ import { icons } from "@/constants/icons";
 const SearchBar = ({
   onPress,
   placeholder,
+  value,
+  onChangeText,
 }: {
   onPress: () => void;
   placeholder?: string;
+  value: string;
+  onChangeText: (text: string) => void;
 }) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
@@ -20,8 +24,8 @@ const SearchBar = ({
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#A8B5DB"
         className="flex-1 ml-2 text-white"
       />
